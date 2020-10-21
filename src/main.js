15e11e581695f4a98010529b1675039ff11762df
zzdl <2547796262@qq.cn>
2020-10-21 23:35:36 +0800

    初始化项目完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入全局组件
+import './styles/index.scss'
+// 导入 vant核心组件库
+import Vant from 'vant'
+// 导入 vant组件库样式
+import 'vant/lib/index.css'
+// 导入rem适配 包
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
